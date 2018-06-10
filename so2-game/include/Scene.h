1bcb45463a21a2f9e3d0070f59ac91b5130b1f79
Jan Potocki <jpotocki32@gmail.com>
2018-06-10 17:22:44 +0200

    so2-game: refactoring in progress

--- so2-game/include/Scene.h
+++ so2-game/include/Scene.h
@@ -1,18 +1,24 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
 
 class Scene
 {
     public:
         Scene();
         virtual ~Scene();
-        static int GetxMax() { return xMax; }
-        void SetxMax(static int val) { xMax = val; }
-        static int GetyMax() { return yMax; }
-        void SetyMax(static int val) { yMax = val; }
+
+        //static int GetyMax() { return yMax; }
+        //void SetyMax(static int val) { yMax = val; }
     protected:
     private:
+        static bool initialized;
+        static bool freezed;
+        static std::mutex freezeMutex;
+        static std::condition_variable freezeCondition;
         static int xMax;
         static int yMax;
 };
